9eb906db9fdb5dfab736285036cf263c62eb147d
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-18 10:29:45 -0400

    Prevent page from refreshing on submission

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
   function handleChange(event) {
     console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeading(name);
+    event.preventDefault();
   }
 
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input
-        onChange={handleChange}
-        type="text"
-        placeholder="What's your name?"
-      />
-      <button>Submit</button>
+      <form onSubmit={handleClick}>
+        <h1>Hello {headingText}</h1>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
