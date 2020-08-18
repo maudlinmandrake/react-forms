aaa459a0599c0b6f4274fea7f0d3d17b6d19d24f
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-18 10:03:46 -0400

    Detect value user has typed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 function App() {
+  function handleChange(event) {
+    console.log(event.target.value);
+  }
+
   return (
     <div className="container">
       <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
       <button>Submit</button>
     </div>
   );
